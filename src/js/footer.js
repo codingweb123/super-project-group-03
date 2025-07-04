4e62157b72cf7a1b964d0244dcfd1bdc07b7664d
Vita Inhulska <gavrishenko.vita@gmail.com>
2025-07-04 21:32:14 +0300

    fixed footer

--- src/js/footer.js
+++ src/js/footer.js
@@ -5,15 +5,6 @@ const footerForm = document.querySelector('.form-footer');
 const emailInput = document.querySelector('.footer-email-field');
 const footerBtn = document.querySelector('.footer-btn');
 
-emailInput.addEventListener('input', () => {
-    const value = emailInput.value.trim();
-    if (value.includes('@') && value.length > 3) {
-      footerBtn.disabled = false;
-    } else {
-      footerBtn.disabled = true;
-    }
-});
-
 footerForm.addEventListener('submit', function (event) {
     event.preventDefault();
 
