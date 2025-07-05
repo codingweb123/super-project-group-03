ef7dd73d81d79b639a64669f754dfa38c4995a1b
Edward <vecheryaedik@gmail.com>
2025-07-06 00:33:19 +0300

    fix_ver_1

--- src/js/events.js
+++ src/js/events.js
@@ -1,3 +1,8 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+import { Navigation, Pagination, Scrollbar } from 'swiper/modules';
+Swiper.use([Navigation, Pagination, Scrollbar]);
+
 document.addEventListener('DOMContentLoaded', () => {
   const registerLinks = document.querySelectorAll('.event-register');
   const modalOverlay = document.querySelector('.register-modal');
@@ -5,41 +10,44 @@ document.addEventListener('DOMContentLoaded', () => {
   registerLinks.forEach(link => {
     link.addEventListener('click', e => {
       e.preventDefault();
-      modalOverlay.classList.add('open');
+      modalOverlay.classList.add('is-open');
     });
   });
 
-  // Закрытие модалки при клике по фону
+  // Закрытие модалкy при клике по фону
   modalOverlay.addEventListener('click', e => {
     if (e.target === modalOverlay) {
-      modalOverlay.classList.remove('open');
+      modalOverlay.classList.remove('is-open');
     }
   });
+});
 
-  // Радио кнопки______________________________________________________
-  const radios = Array.from(document.querySelectorAll('input[name="slider"]'));
-  const prevBtn = document.querySelector('#prevBtn');
-  const nextBtn = document.querySelector('#nextBtn');
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  spaceBetween: 24,
+  slidesPerView: 1,
 
-  function getCheckedIndex() {
-    return radios.findIndex(radio => radio.checked);
-  }
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 
-  function setChecked(index) {
-    radios.forEach((radio, i) => {
-      radio.checked = i === index;
-    });
-  }
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
 
-  prevBtn.addEventListener('click', () => {
-    let current = getCheckedIndex();
-    let prev = (current - 1 + radios.length) % radios.length;
-    setChecked(prev);
-  });
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  },
 
-  nextBtn.addEventListener('click', () => {
-    let current = getCheckedIndex();
-    let next = (current + 1) % radios.length;
-    setChecked(next);
-  });
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1440: {
+      slidesPerView: 3,
+    },
+  },
 });
