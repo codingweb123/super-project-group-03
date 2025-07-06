59ec7f0a3b8b763dbdcaad9b33494aee67604feb
Iryna <iryna0425@gmail.com>
2025-07-06 18:39:59 +0300

    loc

---
+++ src/js/location.js
@@ -0,0 +1,15 @@
+  document.addEventListener("DOMContentLoaded", () => {
+    const iframes = document.querySelectorAll('iframe[data-src]');
+
+    const observer = new IntersectionObserver((entries, obs) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const iframe = entry.target;
+          iframe.setAttribute('src', iframe.dataset.src);
+          obs.unobserve(iframe);
+        }
+      });
+    });
+
+    iframes.forEach(iframe => observer.observe(iframe));
+  });
