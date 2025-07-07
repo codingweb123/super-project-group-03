dd459a0f7c112ddcd4dfb56fab71f2be0893f0c0
Iryna <iryna0425@gmail.com>
2025-07-07 05:12:45 +0300

    scroll

--- src/js/location.js
+++ src/js/location.js
@@ -1,15 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const iframes = document.querySelectorAll('iframe[data-src]');
+  document
+    .querySelectorAll('iframe[data-src]')
+    .forEach(iframe => iframe.setAttribute('src', iframe.dataset.src));
+});
 
-  const observer = new IntersectionObserver((entries, obs) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        const iframe = entry.target;
-        iframe.setAttribute('src', iframe.dataset.src);
-        obs.unobserve(iframe);
-      }
-    });
+const scrollBtn = document.querySelector('#scrollToTopBtn');
+
+window.addEventListener('scroll', () => {
+  if (scrollY > 300) {
+    scrollBtn.classList.add('show');
+  } else {
+    scrollBtn.classList.remove('show');
+  }
+});
+
+scrollBtn.addEventListener('click', () => {
+  scrollTo({
+    top: 0,
+    behavior: 'smooth',
   });
-
-  iframes.forEach(iframe => observer.observe(iframe));
 });
