092abc1d5953289320aa640ef56bf5316d3059a9
codingweb123 <120459347+codingweb123@users.noreply.github.com>
2025-07-08 12:35:20 +0300

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import "./js/header"
+import "./js/hero"
+import "./js/books"
+import "./js/feedbacks"
+import "./js/events"
+import "./js/footer"
+import "./js/location"
+import "./js/register-modal"
