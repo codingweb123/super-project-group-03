2fbbc8d1c94d5ae405a7929cecc9c205d7d18e6d
codingweb123 <120459347+codingweb123@users.noreply.github.com>
2025-07-08 14:20:54 +0300

    Update menu-overlay.js

--- src/js/menu-overlay.js
+++ src/js/menu-overlay.js
@@ -1,26 +1,23 @@
-const burgerMenu = document.querySelector(".menu");
-const openBtn = document.querySelector(".burger-btn");
-const menuOverlay = document.querySelector(".menu-overlay");
+const burgerMenu = document.querySelector(".menu")
+const openBtn = document.querySelector(".burger-btn")
+const menuOverlay = document.querySelector(".menu-overlay")
 
-openBtn.addEventListener("click", () => menuOverlay.classList.add("is-open"));
+openBtn.addEventListener("click", () => menuOverlay.classList.add("is-open"))
 
 burgerMenu.addEventListener("click", element => {
-    let menuBtn = element.target;
+	element.preventDefault()
+	let menuBtn = element.target
 
-    if (menuBtn.nodeName === "use" || menuBtn.nodeName === "svg") {
+	if (menuBtn.nodeName === "use" || menuBtn.nodeName === "svg") {
 		menuBtn = menuBtn.closest(".menu-close-btn")
-    }
-
-    if (
-		menuBtn.classList.contains("menu-close-btn") ||
-		menuBtn.classList.contains("menu-nav-link")
-	) {
+	}
+	if (menuBtn.classList.contains("menu-close-btn")) {
 		menuOverlay.classList.remove("is-open")
 	}
+	if (menuBtn.classList.contains("menu-nav-link")) {
+		menuOverlay.classList.remove("is-open")
+		document.querySelector(menuBtn.getAttribute("href")).scrollIntoView({
+			behavior: "smooth",
+		})
+	}
 })
-
-
-
-	
-
-	
